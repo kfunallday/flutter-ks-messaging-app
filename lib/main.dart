cd183712d93f37e78ebf506b5a9b8705540889f6
kfunallday <mywebsiteemail34@gmail.com>
2026-08-17 17:19:42 +0000

    Add firebase_core initialization and update launcher icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'ChatScreen.dart';
 import 'util/Themes.dart';
 
-void main() => runApp(const FlutterChatApp());
+void main() async {
+  // Binds native platform channels before async operations run
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initializes Firebase SDK before rendering UI
+  await Firebase.initializeApp();
+
+  runApp(const FlutterChatApp());
+}
 
 class FlutterChatApp extends StatelessWidget {
   const FlutterChatApp({super.key});
