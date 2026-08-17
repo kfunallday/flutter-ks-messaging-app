2c6adbcf120ec58cde818350cb02a02b245a9d44
kfunallday <mywebsiteemail34@gmail.com>
2026-08-17 14:43:38 +0000

    Fix compileSdk to 36 and update relative imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_app/ChatScreen.dart';
-import 'package:flutter_chat_app/util/Themes.dart';
 
-void main() => runApp(new FlutterChatApp());
+import 'ChatScreen.dart';
+import 'util/Themes.dart';
+
+void main() => runApp(const FlutterChatApp());
 
 class FlutterChatApp extends StatelessWidget {
+  const FlutterChatApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Flutter Chat App",
       theme: defaultTargetPlatform == TargetPlatform.iOS
           ? Themes.kIOSTheme
           : Themes.kDefaultTheme,
-      home: new ChatScreen(),
+      home: const ChatScreen(),
     );
   }
 }
